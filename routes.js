85907409f7c1fa4309d40c9456cbe6853820f1c2
Jose Murilo <josemurilot@gmail.com>
2020-03-14 00:31:54 -0300

    Criação da rota para pagina de cadastro de instrutor, e rota para receber dados do formulario

--- routes.js
+++ routes.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/', function(req, res) {
+routes.get('/', function (req, res) {
     return res.redirect("/instructors")
 })
 
-routes.get('/instructors', function(req, res) {
+routes.get('/instructors', function (req, res) {
     return res.render('instructors/index')
 })
 
-routes.get('/members', function(req, res) {
+routes.get('/instructors/create', function (req, res) {
+    return res.render('instructors/create')
+})
+
+routes.post('/instructors', function (req, res) {
+    return res.send('Recebido...')
+})
+
+routes.get('/members', function (req, res) {
     return res.send('/members')
 })
 
