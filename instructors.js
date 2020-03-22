8b2232995447af09ba607aafb032b08c043f4901
Jose Murilo <josemurilot@gmail.com>
2020-03-22 15:51:01 -0300

    Desestruturado array, dados vindo do req.body

--- instructors.js
+++ instructors.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const data = require('./data.json')
+
 exports.post = function (req, res) {
    
     const keys = Object.keys(req.body)
@@ -8,6 +11,32 @@ exports.post = function (req, res) {
         }
     }
 
-    return res.send(req.body)
+let {avatar_url, name, birth, gender, services} = req.body
+
+  birth = Date.parse(birth)
+  const created_at = Date.now()
+  const id = Number(data.instructors.length + 1)
+
+  
+
+  data.instructors.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at,
+
+  })
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send('Write file error')
+
+        
+        return res.redirect('/instructors')
+    })
+   
+   
 
 }
