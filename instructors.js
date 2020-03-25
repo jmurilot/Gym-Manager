8a0107eec5fccf13299e468f5f377e37477bb174
Jose Murilo <josemurilot@gmail.com>
2020-03-24 23:55:47 -0300

    removido função de data e colocado no arquivo util.js

--- instructors.js
+++ instructors.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const data = require("./data.json")
+const { age } = require('./utils.js')
+const Intl = require("intl")
 
 exports.post = function (req, res) {
    
@@ -51,28 +53,12 @@ exports.show = function(req, res) {
     })
 
     if (!foundInstructor) return res.send("Instructor not Found!!!")
-
-    function age(timestamp) {
-        const today = new Date()
-        const birthDate = new Date(timestamp)
-
-        let age = today.getFullYear() - birthDate.getFullYear()
-        const month = today.getMonth() - birthDate.getMonth()
-
-        if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
-            age = age - 1
-        }
-        return age
-            
-
-    }
-
-
+    
     const instructor = {
         ...foundInstructor,
         age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at: ""
+        created_at: new Intl.DateTimeFormat('pt-br').format(foundInstructor.created_at),
     }
 
     return res.render('./instructors/show', {instructor})
